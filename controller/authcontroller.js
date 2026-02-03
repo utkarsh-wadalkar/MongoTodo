4c93da59c54dee6b691c69eb657543d1279e18d5
utkarsh-wadalkar <utkarshw1625@gmail.com>
2026-02-04 04:53:23 +0530

    authcontroller

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -22,47 +22,47 @@ async function register(req, res) {
 
   } catch (err) {
     console.log(err);
-    res.status(400).send(err);
+    res.status(500).send({ error: 'Server error during registration. Please try again.' });
   }
 }
 
 //LOGIN
-async function login(req, res){
-  try{
-    const {username,password} = req.body;
-    const user = await User.findOne({username});
-        if(!user){
-          return res.status(400).send({error:"Username not found"})
-        }
-
-        const passValid = await user.comparePass(password)
-        if(!passValid){
-          return res.status(400).send({error:"Invalid Password"})
-        }
-        //if both is valid then create token jwt.sign()
-
-        let token = jwt.sign({userId:user._id}, JWT_SECRET, {expiresIn:'24h'})
-        
-        let checkData = {        //to check what is being requested and send it to backend
-          userId: user?._id,
-          name: user?.name,
-          username: user?.username,
-          token
-        }
-        
-        res.status(201).send({
-          message: "Login Successful",
-          userdata: checkData
-        });
-
-    }catch(err){
-      console.log(err);
-      res.status(400).send(err)
+async function login(req, res) {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).send({ error: "Username not found" })
     }
+
+    const passValid = await user.comparePass(password)
+    if (!passValid) {
+      return res.status(400).send({ error: "Invalid Password" })
+    }
+    //if both is valid then create token jwt.sign()
+
+    let token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '24h' })
+
+    let checkData = {        //to check what is being requested and send it to backend
+      userId: user?._id,
+      name: user?.name,
+      username: user?.username,
+      token
+    }
+
+    res.status(201).send({
+      message: "Login Successful",
+      userdata: checkData
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: 'Server error during login. Please try again.' })
+  }
 }
-const authcontroller ={
-    register,
-    login
+const authcontroller = {
+  register,
+  login
 }
 
 module.exports = authcontroller;
